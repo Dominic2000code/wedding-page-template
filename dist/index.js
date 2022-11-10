cb23158d77aa0953373ac010d7f65c58c3f16adb
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-10 17:06:19 +0000

    made input type submit

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@ const button = document.querySelector(".rsvp-btn");
 const modalBg = document.querySelector(".modal-bg");
 const modalClose = document.querySelector(".modal-close");
 const rsvpButton = document.querySelector(".btn_rsvp");
+const rsvpInput = document.querySelector(".rsvp_input");
 
 button.addEventListener("click", () => {
   modalBg.classList.add("bg-active");
@@ -11,7 +12,11 @@ modalClose.addEventListener("click", () => {
   modalBg.classList.remove("bg-active");
 });
 
-rsvpButton.addEventListener("click", () => {
+rsvpButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  const name = rsvpInput.value;
   modalBg.classList.remove("bg-active");
-  alert("We will be expecting you");
+
+  alert(`${name}, we will be expecting you`);
+  rsvpInput.value = "";
 });
