076f195da34ae3e167a7fc9731c535b0c78f216f
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-10 08:30:54 +0000

    added event listener to rsvp button

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,17 @@
 const button = document.querySelector(".rsvp-btn");
 const modalBg = document.querySelector(".modal-bg");
 const modalClose = document.querySelector(".modal-close");
+const rsvpButton = document.querySelector(".btn_rsvp");
 
 button.addEventListener("click", () => {
-	modalBg.classList.add("bg-active");
+  modalBg.classList.add("bg-active");
 });
 
 modalClose.addEventListener("click", () => {
-	modalBg.classList.remove("bg-active");
+  modalBg.classList.remove("bg-active");
+});
+
+rsvpButton.addEventListener("click", () => {
+  modalBg.classList.remove("bg-active");
+  alert("We will be expecting you");
 });
